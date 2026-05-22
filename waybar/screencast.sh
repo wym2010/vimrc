71de7ba1b1cb730999802c7ac93526ad5ea20102
wym2010 <1035381759@qq.com>
2026-05-22 08:40:59 +0800

    update sway and waybar config: fix screencast ECS cancel, add grim screenshot

--- waybar/screencast.sh
+++ waybar/screencast.sh
@@ -5,6 +5,7 @@ mkdir -p "$RECORDING_DIR"
 if pgrep -x wf-recorder >/dev/null; then
     pkill -x wf-recorder
 else
+    geometry=$(slurp) || exit 1
     file="$RECORDING_DIR/recording_$(date +%Y%m%d_%H%M%S).mp4"
-    wf-recorder -g "$(slurp)" -f "$file" &
+    wf-recorder -g "$geometry" -f "$file" &
 fi
